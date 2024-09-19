50362a2da6d58ec7cbe5654cea9a8960746f311e
RASIUT <rrout9240@gmail.com>
2024-09-20 00:01:58 +0530

    sidebar added

--- CryptoTrading_FrontEnd/src/page/Navbar/Navbar.jsx
+++ CryptoTrading_FrontEnd/src/page/Navbar/Navbar.jsx
@@ -1,36 +1,43 @@
 import { Avatar } from '@/components/ui/avatar'
 import { Button } from '@/components/ui/button'
-import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from '@/components/ui/sheet'
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from '@/components/ui/sheet'
 import { AvatarImage } from '@radix-ui/react-avatar'
-import { AvatarIcon, DragHandleHorizontalIcon } from '@radix-ui/react-icons'
+import { DragHandleHorizontalIcon } from '@radix-ui/react-icons'
+// import { CoinDasher } from './CoinDasher.jpg'
 import React from 'react'
+import Sidebar from './Sidebar'
 
 const Navbar = () => {
-  return (
-	<div className='px-2 py-3 border-b z-50 bg-background bg-opacity-0 sticky top-0 left-0 rigth-0 flex justify-between item-center'>
-		<div className='flex item-center gap-3'>
-			<Sheet>
-				<SheetTrigger>
-					<Button variant="ghost" size="icon" className="rounded-full h-11 w-11">
-						<DragHandleHorizontalIcon className="h-7 w-7"/>
-					</Button>
-				</SheetTrigger>
-				<SheetContent className="w-72 border-r-0 flex felx-col justify-center" side="left">
-					<SheetHeader>
-						<SheetTitle>
-							<div className="text-3xl flex justify-center items-center gap-1">
-								<Avatar>
-									<AvatarIcon src="https://www.cdnlogo.com/logos/c/21/coinbene.svg"/>
-								</Avatar>
-							</div>
-						</SheetTitle>
-						
-					</SheetHeader>
-				</SheetContent>
-			</Sheet>
+	return (
+		<div className='px-2 py-3 border-b z-50 bg-background bg-opacity-0 sticky top-0 left-0 right-0 flex justify-between items-center'>
+			<div className='flex items-center gap-3'>
+				<Sheet>
+					<SheetTrigger>
+						<Button variant="ghost" size="icon" className="rounded-full h-11 w-11">
+							<DragHandleHorizontalIcon className="h-7 w-7" />
+						</Button>
+					</SheetTrigger>
+					<SheetContent className="w-72 border-r-0 flex flex-col justify-center" side="left">
+						<SheetHeader>
+							<SheetTitle>
+								<div className="text-3xl flex justify-center items-center gap-1">
+									<Avatar>
+										{/* If you have an image, use AvatarImage */}
+										{/* <AvatarImage src={CoinDasher} alt="CoinDasher" /> */}
+									</Avatar>
+									<div>
+										<span className='font-bold text-amber-700'>Cryp</span>
+										<span>T!fy</span>
+									</div>
+								</div>
+							</SheetTitle>
+						</SheetHeader>
+						<Sidebar/>
+					</SheetContent>
+				</Sheet>
+			</div>
 		</div>
-	</div>
-  )
+	)
 }
 
 export default Navbar
