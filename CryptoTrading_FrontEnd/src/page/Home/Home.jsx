27c514dfc95e7fd47cd97462201d8a0ace37730b
RASIUT <rrout9240@gmail.com>
2024-09-20 16:11:34 +0530

    chatbot added

--- CryptoTrading_FrontEnd/src/page/Home/Home.jsx
+++ CryptoTrading_FrontEnd/src/page/Home/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import AssetTable from './AssetTable'
 import CoinChart from './CoinChart'
 import { Avatar, AvatarImage } from '@radix-ui/react-avatar'
-import { DotIcon } from '@radix-ui/react-icons'
+import { Cross1Icon, DotIcon } from '@radix-ui/react-icons'
 import { MessageCircle } from 'lucide-react'
 
 const Home = () => {
@@ -53,8 +53,43 @@ const Home = () => {
 				</div>
 			</div>
 
-			{/* Chat Bot Button with fixed position */}
 			<section className='fixed bottom-5 right-5 z-40 flex flex-col justify-end items-end gap-2'>
+
+				<div className="rounded-md w-[20rem] md:w-[25rem] lg:w-[25rem] h-[70vh] bg-slate-900">
+					<div className="flex justify-between items-center border-b px-6 h-[12%]">
+						<p>Chat Bot</p>
+						<Button variant="ghost" size="icon">
+							<Cross1Icon/>
+						</Button>
+					</div>
+					<div className="h-[76%] flex flex-col overflow-y-auto gap-5 px-5 py-2 scroll-container">
+						<div className="self-start pb-5 w-auto">
+							<div className='justify-end self-end px-5 py-2 rounded-md bg-slate-800 w-auto'>
+								<p>Hi, RASIUT</p>
+								<p>You can ask any question realted to crypto</p>
+								<p>like price, market cap, etc.</p>
+							</div>
+						</div>
+
+						{
+							[1, 1, 1, 1].map((item, i) => (
+								<div key = {i} className={`${i%2==0 ? "self-start" : "self-end"} pb-5 w-auto`}>
+									{i % 2 == 0 ? 
+									<div className='justify-end self-end px-5 py-2 rounded-md bg-slate-800 w-auto'>
+										<p>prompt who are you</p>
+									</div>
+									:
+									<div className='justify-end self-end px-5 py-2 rounded-md bg-slate-800 w-auto'>
+										<p>ans Hi, RASIUT</p>
+									</div>
+								}
+									
+								</div>
+							))
+						}
+
+					</div>
+				</div>
 				<div className='relative w-[10rem] cursor-pointer group'>
 					<Button className="w-full h-[3rem] gap-2 items-center">
 						<MessageCircle size={30} className='fill-[#1E293B] -rotate-90 -stroke-none group-hover:fill-[#1a1a1a]' />
